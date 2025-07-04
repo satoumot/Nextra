6958aa62a30b16347864ce13aa097cc6afef2474
satoumot <satoumot@systena.co.jp>
2025-07-04 14:33:45 +0900

    search

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Footer, Layout, Navbar } from 'nextra-theme-docs'
 import { Banner, Head } from 'nextra/components'
 import { getPageMap } from 'nextra/page-map'
@@ -5,10 +6,10 @@ import 'nextra-theme-docs/style.css'
 import '@docsearch/css'
 import { DocSearch } from '@docsearch/react';
 
-export const metadata = {
-  // Define your metadata here
-  // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
-}
+// export const metadata = {
+//   // Define your metadata here
+//   // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
+// }
  
 const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>
 const navbar = (
